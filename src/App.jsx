3636ffb92d6582de547e869e92d3351143085c58
IvanScriptS <iachavez@utp.edu.co>
2022-11-29 16:49:59 -0500

    Se muestra una alerta y se empieza + Props

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import Formulario from "./components/Formulario"
+import Header from "./components/header"
+import ListadoPacientes from "./components/ListadoPacientes"
+
+
+function App() {
+
+  const [pacientes, setPacientes]=useState([])
+
+  return (
+    <div className="container mx-auto mt-10">
+      <Header />
+
+      <div className="mt-12 md:flex">
+        <Formulario />
+        <ListadoPacientes />
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Pacientes.jsx
@@ -0,0 +1,33 @@
+
+const Pacientes = () => {
+  return (
+    <div className="bg-white mx-5 my-10 shadow-md px-5 py-10 rounded-md">
+                <p className="font-bold mb-3 text-gray-700 rounded-xl">Nombre: {""}
+
+                    <span className="font-normal normal-case">Hook</span>
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 rounded-xl">Propietario: {""}
+
+                    <span className="font-normal normal-case">Iván Chávez</span>
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 rounded-xl">E-mail: {""}
+
+                    <span className="font-normal normal-case">iachavez@utp.edu.co</span>
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 rounded-xl">Fecha Alta: {""}
+
+                    <span className="font-normal normal-case">11 de diciembre 2021</span>
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 rounded-xl">Síntomas: {""}
+
+                    <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur officia culpa facilis ut voluptates rem, maxime nostrum ipsum, at velit illo pariatur molestias architecto temporibus voluptatem? Saepe alias id incidunt?</span>
+                </p>
+    </div>
+  )
+}
+
+export default Pacientes
